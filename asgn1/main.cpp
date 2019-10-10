8c5d9ddcadc5b2fbaae8f42152a7fc8bad0fcd88
Max <max.nibler@gmail.com>
2019-10-10 14:38:12 +0000

    implementing cpp

---
+++ asgn1/main.cpp
@@ -0,0 +1,38 @@
+#include <cerrno>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+const string CPP = "/usr/bin/cpp -nostdinc";
+constexpr size_t buffSize = 1024;
+void removeChar(char* line, char target){
+  size_t slen = strlen(line);
+  if (slen == 0)return;
+  char* pos = line + slen - 1;
+  if (*pos == target)*pos = '\0';
+}
+void preProc(FILE* pipe, string filename){
+  char Buff[buffSize];
+  const char* readLine = fgets(Buff, buffSize, pipe);
+  removeChar(Buff, '\n');
+  cout << "FirstLine: " << Buff << endl;
+}
+
+int main(int argc, char* argv[]){
+  string inFile = argv[argc-1];
+  if ( inFile[inFile.length()-1] != 'c' ||
+       inFile[inFile.length()-2] != 'o' ||
+       inFile[inFile.length()-3] != '.'){
+    cerr << "error: " << inFile << " is not an .oc file\n";
+    exit(-1);
+  }
+  string com = CPP + " " + inFile;
+  cout << "command=\"" << com << "\"" << endl;
+  FILE* pipe = popen (com.c_str(), "r");
+  int fileCloseStat = pclose(pipe);
+  preProc(pipe, inFile);
+  return 0;
+}
