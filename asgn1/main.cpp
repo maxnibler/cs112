391c3f41a27c4639c330bfbd2d9ca7dd33956c3b
Max <max.nibler@gmail.com>
2019-10-10 17:18:58 +0000

    v1 complete

--- asgn1/main.cpp
+++ asgn1/main.cpp
@@ -2,8 +2,10 @@
 #include <cstring>
 #include <iomanip>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <string.h>
+#include "string_set.h"
 using namespace std;
 
 const string CPP = "/usr/bin/cpp -nostdinc";
@@ -15,6 +17,7 @@ void removeChar(char* line, char target){
   if (*pos == target)*pos = '\0';
 }
 void preProc(FILE* pipe, string filename){
+  string_set strSet;
   char Buff[buffSize];
   int lineNum = 1;
   while(true){
@@ -25,28 +28,40 @@ void preProc(FILE* pipe, string filename){
     char* saveLine = nullptr;
     char* linePtr = Buff;
     for(int tokCount = 1;; ++tokCount){
-      char* token = strtok_r(linePtr, " \t\n", &saveLine);
+      const char* token = strtok_r(linePtr, " \t\n", &saveLine);
       linePtr = nullptr;
       if (token == nullptr) break;
-      cout << lineNum << "; " << tokCount << ": " <<
-	token << endl;
+      // cout << lineNum << "; " << tokCount << ": " <<
+      //	token << endl;
+      const string* returnStr = strSet.intern(token);
     }
     ++lineNum;
   }
+  cout << filename << endl;
+  filebuf outBuff;
+  outBuff.open(filename, ios::out);
+  ostream outStream(&outBuff);
+  strSet.dump(outStream);
+  outBuff.close();
 }
 
 int main(int argc, char* argv[]){
+  string com;
   string inFile = argv[argc-1];
   if ( inFile[inFile.length()-1] != 'c' ||
        inFile[inFile.length()-2] != 'o' ||
        inFile[inFile.length()-3] != '.'){
     cerr << "error: " << inFile << " is not an .oc file\n";
     exit(-1);
+  }else{
+    com = CPP + " " + inFile;
+    inFile[inFile.length()-2] = 's';
+    inFile[inFile.length()-1] = 't';
   }
-  string com = CPP + " " + inFile;
-  cout << "command=\"" << com << "\"" << endl;
+  //cout << "command=\"" << inFile << "\"" << endl;
+  string filename = inFile+"r";
   FILE* pipe = popen (com.c_str(), "r");
-  preProc(pipe, inFile);
+  preProc(pipe, filename);
   int fileCloseStat = pclose(pipe);
   return 0;
 }
